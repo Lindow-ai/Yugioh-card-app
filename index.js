ef45859c8f335b9e5bb3db08c0b23933658b395d
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2021-03-27 15:53:10 +0100

    loop name of the map list

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const fetch = require('node-fetch')
 const path = require('path')
 
 const PORT = process.env.PORT || 5003
 
 const app = express()
 
+const getAllCard = async () => {
+    try {
+    const res = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?startdate=Jan%2001%202000&&enddate=Jan%2001%202004&&language=fr')
+    const json = await res.json()
+    console.table(json.data)
+    return json 
+} catch (err) {
+    console.log(err)
+}
+} 
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.engine('.hbs', exphbs({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
-app.get('/', (req, res) => res.render('home', { title: 'Home', subTitle: 'Ma home page' }))
+app.get('/', async(req, res) => {
+    try {
+        const card = await getAllCard()
+        res.render('home', {card})
+    } catch (err) {
+        console.log(err)
+    }
+})
 
 app.get('/:title', (req, res) => {
     const title = req.params.title
